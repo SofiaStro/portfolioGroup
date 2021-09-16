b57d8aced8646365a7a8d531f69a1a0ea85481a9
Sofia Strömberg <83826297+SofiaStro@users.noreply.github.com>
2021-09-16 15:48:39 +0200

    header och footer klar

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 $(document).ready(function(){
 
-    
+    //---------Hover över meny-------------
+    let $navEl = $('#navList a')
+    $navEl.mouseenter(function(e){
+        $(e.target).css('background-color', '#D4AF37');
+    })
+    $navEl.mouseleave(function(e){
+        $(e.target).css('background-color', '#d4d4d4');
+    })
+
+    //---------Öpnna och stäng meny i mobil version-------------
+    $('#navBox').click(function(){
+        let $nav = $('#navList');
+        
+        if($nav.hasClass('unvisible')){
+            $nav.removeClass('unvisible');
+            $nav.addClass('visible'); 
+            $('#navBoxImg').attr('src', 'img/navX.png');
+        }else{
+            $nav.removeClass('visible');
+            $nav.addClass('unvisible');
+            $('#navBoxImg').attr('src', 'img/nav.png');
+        };
+    });
 });
